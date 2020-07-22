a02b9eebbc09ad7335d3cc26677f0fb787f7b604
Betty Flores <beatriz.floresr@ucuenca.edu.ec>
2020-07-22 12:24:51 -0500

    validar

--- registro.php
+++ registro.php
@@ -11,10 +11,6 @@
 </head>
 
 <body>
-
-
-
-
     <?php include('header1.php'); ?>
     <main class="container">
         <div class="row mt-4" style="margin-bottom: 100px;">
@@ -22,32 +18,42 @@
                 <div style="display: flex;flex-direction: column; margin: auto auto;">
                     <h2 style="display: block;text-align: center;">Se parte de nuestra familia Zibá</h2>
                     <hr>
-                    <form action="index.php" method="POST">
+                    <form action="registro.php" method="POST" class="needs-validation" novalidate>
                         <div class="form-group row px-4 mb-2">
                             <div class="col-12 col-md-6">
                                 <label for="nombre">Nombre</label>
-                                <input type="text" class="form-control" placeholder="Ingresa tus nombre" name="nombre" id="nombre">
+                                <input type="text" class="form-control" placeholder="Ingresa tus nombre" name="nombre" id="nombre" required>
+                                <div class="valid-feedback">Válido</div>
+                                <div class="invalid-feedback">Complete el campo</div>
                             </div>
                             <div class="col-12 col-md-6">
                                 <label for="apellido">Apellido</label>
-                                <input type="text" class="form-control" placeholder="Ingresa tus apellidos" name="apellido" id="apellido">
+                                <input type="text" class="form-control" placeholder="Ingresa tus apellidos" name="apellido" id="apellido" required>
+                                <div class="valid-feedback">Válido</div>
+                                <div class="invalid-feedback">Complete el campo</div>
                             </div>
 
                         </div>
                         <div class="form-group row px-4 mb-2">
                             <div class="col-12 col-md-6">
                                 <label for="numeroD">Usuario</label>
-                                <input type="text" class="form-control" placeholder="Ingresa un nombre de usuario" name="usuario" id="usuario">
+                                <input type="text" class="form-control" placeholder="Ingresa un nombre de usuario" name="usuario" id="usuario" required>
+                                <div class="valid-feedback">Válido</div>
+                                <div class="invalid-feedback">Complete el campo</div>
                             </div>
                             <div class="col-12 col-md-6">
                                 <label for="email">Correo electrónico</label>
-                                <input type="email" class="form-control" placeholder="Ingresa tu correo electrónico" name="correo" id="correo">
+                                <input type="email" class="form-control" placeholder="Ingresa tu correo electrónico" name="correo" id="correo" required>
+                                <div class="valid-feedback">Válido</div>
+                                <div class="invalid-feedback">Complete el campo</div>
                             </div>
                         </div>
                         <div class="form-group row px-4 mb-2">
                             <div class="col-12 col-md-6 mb-3">
                                 <label for="contraseña">Contraseña</label>
-                                <input type="password" class="form-control" placeholder="Ingresa tu contraseña" name="contrasena" id="contrasena">
+                                <input type="password" class="form-control" placeholder="Ingresa tu contraseña" name="contrasena" id="contrasena" required>
+                                <div class="valid-feedback">Válido</div>
+                                <div class="invalid-feedback">Complete el campo</div>
                             </div>
                         </div>
                         <div class="form-group row px-4 mb-2">
@@ -56,11 +62,12 @@
                                     <input type="checkbox" name="terminos" id="terminos" class="form-check-input mr-2">
                                     He leído y acepto los términos y condiciones de uso
                                 </label>
+                                <div class="valid-feedback">Válido</div>
                             </div>
                         </div>
 
                         <div class="col-12 mt-3">
-                            <input type="submit" class="btn btn-dark btn-block " value="Registrarse" name="registrarse" data-toggle="modal" data-target="#exampleModal">
+                            <input type="submit" class="btn btn-dark btn-block " value="Registrarse" name="registrarse">
                         </div>
                     </form>
                     <p style="display: block;text-align: center;">O</p>
@@ -77,21 +84,7 @@
             </div>
         </div>
     </main>
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    Usuario registrado
-                </div>
-            </div>
-        </div>
-    </div>
+
     <div style="display: none;">
         <?php
         $data = array(
@@ -125,6 +118,26 @@
     </div>
     <?php include('footer.php'); ?>
 
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
     <script src="validar-registro.php"></script>
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/popper.min.js"></script>
