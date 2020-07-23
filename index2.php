ff91285050662d9bbefbea6caf024793ec6246c9
Betty Flores <beatriz.floresr@ucuenca.edu.ec>
2020-07-23 13:32:09 -0500

    facebook

--- index2.php
+++ index2.php
@@ -88,10 +88,10 @@
     $helper = $fb->getRedirectLoginHelper();
 
     $permissions = ['email']; // Optional permissions
-    $redirectURL = "https://" . $_SERVER['SERVER_NAME'] . "/fb-callback.php";
+    $redirectURL = "https://zibareal.herokuapp.com" . "/fb-callback.php";
     $loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
     echo '<a href="' . $loginUrl . '">Log in con Facebook!</a>';
-
+    
     ?>
 
 
